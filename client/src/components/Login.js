b688180f0fa3f7405ede97a0f95aa17c223174df
Fernando Chavez <fjchavez01@gmail.com>
2020-11-13 09:15:44 -0800

    finished login and got data to appear on bubble page with axiosWithAuth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
+
+const intialState = {
+  username:'',
+  password: ''
+}
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [formValues, setFormValues] = useState(intialState)
+  const { push } = useHistory()
+  const onChange = e => {setFormValues({
+    ...formValues, [e.target.name]: e.target.value
+  })
+    console.log(formValues)
+  }
+  const onSubmit = e => {
+    e.preventDefault()
+    axios.post('http://localhost:5000/api/login', formValues)
+      .then(res => {
+        console.log('response:', res)
+        localStorage.setItem('token', res.data.payload)
+        push('/bubblepage')
+      }).catch( err => {
+        console.log('error', err)
+      })
+
+  }
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+  <div>
+    <form onSubmit={onSubmit}>
+      <label>Username: </label>
+      <input value={formValues.username} onChange={onChange} name='username' type='text'/>
+      <label>Password: </label>
+      <input value={formValues.password} onChange={onChange} name='password' type='text'/>
+      <button>Sign In</button>
+    </form>
+  </div>
   );
 };
 
